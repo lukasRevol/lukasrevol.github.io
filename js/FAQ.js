cfb8fdc3f6a10790a908e74a433546607fa192a9
Natalia <mientuska00@gmail.com>
2023-10-21 14:42:48 +0200

    created FAQ function

--- js/FAQ.js
+++ js/FAQ.js
@@ -1,9 +1,33 @@
+//-------------------------------------
+// FAQ function
+//-------------------------------------
+
 const questions = document.querySelectorAll('.faq__questions--question');
 const answers = document.querySelectorAll('.faq__questions--answer');
+// pobranie wszystkich pytań i odpowiedzi
 
 questions.forEach((question, index) => {
+	const arrow = question.querySelector('.fa-solid');
+	let answer = answers[index]; //nadanie odpowiedzi indexu
+
 	question.addEventListener('click', () => {
-		console.log(`hi`);
-		answers[index].classList.toggle('active');
+		//nadanie nasluchiwania na klik na pytanie dla konkretnej odpowiedzi
+		answers.forEach((otherAnswer, i) => {
+			if (i !== index && otherAnswer.classList.contains('active')) {
+				otherAnswer.classList.remove('active');
+				questions[i].querySelector('.fa-solid').style.transform =
+					'rotate(0deg)';
+			}
+			//jeśli konkretne pytanie nie jest już wyświetlane to je schowaj i obróć strzalkę
+		});
+
+		answer.classList.toggle('active'); //togglowanie widoczności odpowiedzi
+
+		if (arrow.style.transform === 'rotate(180deg)') {
+			arrow.style.transform = 'rotate(0deg)';
+		} else {
+			arrow.style.transform = 'rotate(180deg)';
+		}
+		//funkcja sterująca strzalką
 	});
 });
